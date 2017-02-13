0b91ef0d799ca44d88ff42e4b8364e0c346d98d2
Дмитрий Курганников <true.masterx2@gmail.com>
2017-02-13 19:18:22 +0300

    Testing... 1,2,3

---
+++ src/main.js
@@ -0,0 +1,10 @@
+import $ from 'jquery';
+
+
+$('<div>').text('Testing!!!').css({
+    position: 'absolute',
+    fontSize: '50px',
+    color: 'red',
+    top: 0,
+    left: 0
+}).appendTo(document.body);
